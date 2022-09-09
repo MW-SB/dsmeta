b2d3ed619aab67eb26d8ba840b98e397cd832b33
Murilo Bianco <murilowagner21@gmail.com>
2022-09-08 21:29:24 -0300

    First commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Jesus é Senhor</h1>
+    <>
+      <h1>Jesus é Senhor</h1>
+      <NotificationButton />
+    </>
   )
 }
 
